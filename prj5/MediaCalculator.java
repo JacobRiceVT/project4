a79d16abc669f572706c54931f9f7197758015fe
HoldenTakashi <Holden22@vt.edu>
2022-11-18 22:35:14 -0400

    InputReader and Sorting Logic for intermediate. Also some other changes but I relaly can't be asked to write them I have been coding for like 8 hours straight

--- prj5/MediaCalculator.java
+++ prj5/MediaCalculator.java
@@ -1,5 +1,8 @@
 package prj5;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author Jung Chang
  * @version 11.18.22
@@ -12,6 +15,7 @@ public class MediaCalculator {
     private Month month2;
     private Month month3;
     private Quarter quarter;
+    private InputReader reader; 
 
     /**
      * Constructor
@@ -19,24 +23,64 @@ public class MediaCalculator {
      * @param months
      *            the lists of months
      */
-    public MediaCalculator(Month[] months) {
+    public MediaCalculator(String fileName) {
+        reader = new InputReader(fileName); 
+        Month[] months = reader.getMonthInfo(); 
         month1 = months[0];
         month2 = months[1];
         month3 = months[2];
         quarter = new Quarter(month1, month2, month2);
     }
 
-
-    public LinkedList<Channel> getSortedChannelByName(LinkedList<Channel> channels) {
-        return null;
+    public LinkedList<Channel> getSortedChannelByName() {
+        LinkedList<Channel> unSorted = quarter.getQuarterChannels();
+        LinkedList<Channel> sorted = new LinkedList<Channel>(); 
+        
+        Channel[] chans = new Channel[unSorted.size()]; 
+        //makes a deep copy
+        for (int i = 0; i < unSorted.size(); i++) { 
+            chans[i] = unSorted.get(i); 
+        }
+        
+        Arrays.sort(chans); 
+        
+        for (int i = 0; i < unSorted.size(); i++) { 
+            sorted.add(chans[i]);
+        }
+        
+        return sorted; 
     }
 
 
-    public LinkedList<Channel> getSortedTraditional(LinkedList<Channel> channels) {
-        return null;
+    public LinkedList<Channel> getSortedTraditional() {
+        LinkedList<Channel> unSorted = quarter.getQuarterChannels();
+        LinkedList<Channel> sorted = new LinkedList<Channel>(); 
+        
+        Channel[] chans = new Channel[unSorted.size()]; 
+        //makes a deep copy
+        for (int i = 0; i < unSorted.size(); i++) { 
+            chans[i] = unSorted.get(i); 
+        }
+        
+        Arrays.sort(chans, traditionalOrder);
+        for (int i = 0; i < unSorted.size(); i++) { 
+            sorted.add(chans[i]);
+        }
+        return sorted; 
     }
 
-
+    private Comparator<Channel> traditionalOrder =  new Comparator<Channel>() {
+        public int compare(Channel c1, Channel c2) {
+            if (c1.getTEngagementRate() > c1.getTEngagementRate()) {
+                return -1; 
+            } else if (c1.getTEngagementRate() < c1.getTEngagementRate()) {
+                return 1; 
+            } else { 
+                return 0; 
+            }
+        }
+    };
+    
     public LinkedList<Channel> getSortedReach(LinkedList<Channel> channels) {
         return null;
     }
--- prj5/Quarter.java
+++ prj5/Quarter.java
@@ -15,6 +15,7 @@ public class Quarter {
     private LinkedList<Channel> month2Channels;
     private LinkedList<Channel> month3Channels;
     private LinkedList<Channel> quarterChannels;
+
     /**
      * Constructor
      * 
@@ -23,59 +24,68 @@ public class Quarter {
      * @param month2
      *            the second month of the quarter
      * @param month3
-     *            the third month of the quarter                     
+     *            the third month of the quarter
      */
-    public Quarter(Month month1,Month month2, Month month3) {
-        this.month1=month1;
-        this.month2=month2;
-        this.month3=month3;
-        month1Channels=month1.getChannels();
-        month2Channels=month2.getChannels();
-        month3Channels=month3.getChannels();
-        quarterChannels=new LinkedList<Channel>();
+    public Quarter(Month month1, Month month2, Month month3) {
+        this.month1 = month1;
+        this.month2 = month2;
+        this.month3 = month3;
+        month1Channels = month1.getChannels();
+        month2Channels = month2.getChannels();
+        month3Channels = month3.getChannels();
+        quarterChannels = new LinkedList<Channel>();
         addMonthlyChannels(month1Channels);
         addMonthlyChannels(month2Channels);
         addMonthlyChannels(month3Channels);
     }
+    
     /**
      * checking if a Channel exists in the quarterChannels list
      * 
-     * @return the index of the existing channel. Returns -1 if it does not exist.
+     * @return the index of the existing channel. Returns -1 if it does not
+     *         exist.
      */
     private int getIndexOf(Channel channel) {
-        for(int i=0;i<quarterChannels.size();i++) {
-            if(quarterChannels.get(i).getChannelName().equals(channel.getChannelName())){
+        for (int i = 0; i < quarterChannels.size(); i++) {
+            if (quarterChannels.get(i).getChannelName().equals(channel
+                .getChannelName())) {
                 return i;
             }
         }
         return -1;
     }
+
+    
+
     /**
      * adding the channels in the months to the general channels of the quarter
      */
-    private void addMonthlyChannels(LinkedList<Channel>monthChannels) {
-        for(int i=0;i<monthChannels.size();i++) {
-            Channel channel=monthChannels.get(i);
-            int existsIndex=getIndexOf(channel);
-            if(existsIndex==-1) {
+    private void addMonthlyChannels(LinkedList<Channel> monthChannels) {
+        for (int i = 0; i < monthChannels.size(); i++) {
+            Channel channel = monthChannels.get(i);
+            int existsIndex = getIndexOf(channel);
+            if (existsIndex == -1) {
                 quarterChannels.add(channel);
             }
             else {
-                Channel exists=quarterChannels.get(existsIndex);
-                exists.setLikes(exists.getLikes()+channel.getLikes());
-                exists.setPosts(exists.getPosts()+channel.getPosts());
+                Channel exists = quarterChannels.get(existsIndex);
+                exists.setLikes(exists.getLikes() + channel.getLikes());
+                exists.setPosts(exists.getPosts() + channel.getPosts());
                 exists.setFollowers(channel.getFollowers());
-                exists.setComments(exists.getComments()+channel.getComments());
-                exists.setViews(exists.getViews()+channel.getViews());
+                exists.setComments(exists.getComments() + channel
+                    .getComments());
+                exists.setViews(exists.getViews() + channel.getViews());
             }
         }
     }
+
+
     /**
      * getting the quarterChannels list
      * 
      * @return quarteChannels
      */
-    public LinkedList<Channel> getQuarterChannels(){
+    public LinkedList<Channel> getQuarterChannels() {
         return quarterChannels;
     }
 }
